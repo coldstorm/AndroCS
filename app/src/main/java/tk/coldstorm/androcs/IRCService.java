be89392ef9ed090ca1dac3fc14800cc51139f2c6
Express50 <press.ex50@gmail.com>
2014-07-17 17:56:33 -0400

    Add barebones IRCService

---
+++ app/src/main/java/tk/coldstorm/androcs/IRCService.java
@@ -0,0 +1,30 @@
+package tk.coldstorm.androcs;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+/**
+ * A service that holds a connection to an IRC server and relays parsed messages to bound components.
+ * */
+public class IRCService extends Service {
+    private final IBinder mBinder = new IRCBinder();
+
+    /**
+     * A Binder class used to expose the service to bound components.
+     * */
+    public class IRCBinder extends Binder {
+        IRCService getService() {
+            return IRCService.this;
+        }
+    }
+
+    public IRCService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+}
