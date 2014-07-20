076bf961b9bb6609f3ccb5657c3f82e815b62e88
Express50 <press.ex50@gmail.com>
2014-07-20 11:25:49 -0400

    Make IRCService an IntentService

--- app/src/main/java/tk/coldstorm/androcs/services/IRCService.java
+++ app/src/main/java/tk/coldstorm/androcs/services/IRCService.java
@@ -1,30 +1,71 @@
 package tk.coldstorm.androcs.services;
 
-import android.app.Service;
+import android.app.IntentService;
 import android.content.Intent;
-import android.os.Binder;
-import android.os.IBinder;
+import android.content.Context;
+
+import tk.coldstorm.androcs.models.irc.User;
 
 /**
- * A service that holds a connection to an IRC server and relays parsed messages to bound components.
- * */
-public class IRCService extends Service {
-    private final IBinder mBinder = new IRCBinder();
+ * An {@link IntentService} subclass for handling IRC tasks.
+ */
+public class IRCService extends IntentService {
+    //region Actions
+    private static final String ACTION_CONNECT = "tk.coldstorm.androcs.services.action.CONNECT";
+    //endregion
 
+    //region Parameters
+    private static final String EXTRA_ADDRESS = "tk.coldstorm.androcs.services.extra.ADDRESS";
+    private static final String EXTRA_PORT = "tk.coldstorm.androcs.services.extra.PORT";
+    private static final String EXTRA_CLIENT = "tk.coldstorm.androcs.services.extra.CLIENT";
+    //endregion
+
+    //region Action Helpers
     /**
-     * A Binder class used to expose the service to bound components.
-     * */
-    public class IRCBinder extends Binder {
-        IRCService getService() {
-            return IRCService.this;
-        }
+     * Starts this service to perform a Connect action to an IRC server with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @param context       The Context of the Intent
+     * @param serverAddress The host of the server to connect to
+     * @param serverPort    The port to connect to on the server
+     * @param client        The User object to use in IRC registration
+     * @see IntentService
+     */
+    public static void startActionConnect(Context context, String serverAddress, String serverPort, User client) {
+        Intent intent = new Intent(context, IRCService.class);
+        intent.setAction(ACTION_CONNECT);
+        intent.putExtra(EXTRA_ADDRESS, serverAddress);
+        intent.putExtra(EXTRA_PORT, serverPort);
+        intent.putExtra(EXTRA_CLIENT, client);
+        context.startService(intent);
     }
+    //endregion
 
     public IRCService() {
+        super("IRCService");
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        return mBinder;
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_CONNECT.equals(action)) {
+                final String address = intent.getStringExtra(EXTRA_ADDRESS);
+                final String port = intent.getStringExtra(EXTRA_PORT);
+                final User client = intent.getParcelableExtra(EXTRA_CLIENT);
+                handleActionConnect(address, port, client);
+            }
+        }
     }
+
+    //region Action Handlers
+    /**
+     * Handle action Connect in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionConnect(String address, String port, User client) {
+        // TODO: Handle action Connect
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    //endregion
 }
